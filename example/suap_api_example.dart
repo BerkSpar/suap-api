d63aa77fbb7906290e4d9a4a98fd6b5ea27fb90b
Felipe Passos <47111228+BerkSpar@users.noreply.github.com>
2020-09-15 03:43:07 -0300

    setup project

---
+++ example/suap_api_example.dart
@@ -0,0 +1,5 @@
+import 'package:suap_api/suap_api.dart';
+
+void main() {
+  //local onde é mostrado o exemplo do pub.dev
+}
---
+++ lib/suap_api.dart
@@ -0,0 +1,5 @@
+//SUAP Api - Dart Package
+library suap_api;
+
+export 'src/suap_api_base.dart';
+export 'src/suap_api_model.dart';
